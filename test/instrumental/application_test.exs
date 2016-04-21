e6ffb8dc63c0b1267fdb73c398839b0b6e660ad5
Matthew Wade Gordon <matt@expectedbehavior.com>
2016-04-21 13:14:56 -0400

    Improved tests
    Added a test server to be able to compare what we think we're sending to what we're actually sending.
    
    Pros
    - can actually assert correctness
    
    Cons
    - ugly and slow
    - really ugly and slow
    - doesn't (yet) re-implement all the previous tests

--- test/instrumental/application_test.exs
+++ test/instrumental/application_test.exs
@@ -4,45 +4,68 @@ defmodule Instrumental.MetricTest do
   alias Instrumental.Connection
   alias Instrumental.Protocol
   use ExUnit.Case
+  Code.require_file "test/test_server.exs"
+  require Logger
 
-
-  setup_all do
-    {:ok, body} = File.read("test_key")
-    Application.put_env(Instrumental.Config.app, :token, body)
-    {:ok, pid} = Connection.start_link
-    :timer.sleep(5000)
-  end
-
-  # {:ok, cmd} = Protocol.format(:gauge, "elixir.gauge", 1, Time.unix_monotonic)
-  # :ok = Connection.send_cmd(cmd)
-  # Connection.handle_cast({:send, cmd}, Connection.State{sock: pid, })
-  # :timer.sleep(5000)
-  # :ok = Connection.send_cmd(cmd)
-  # {:ok, cmd} = Protocol.format(:gauge, "elixir.gauge", 1, Time.unix_monotonic)
-  # state = %Connection.State{state: :connected}
-  # I.Connection.hand_cast({:send, cmd}, state)
 
   # :dbg.tracer
   # :dbg.p self()
 
-  
   test "sends gauge correctly" do
+    {:ok, server} = KVServer.start(1,self)
+    {:ok, body} = File.read("test_key")
+    Application.put_env(Instrumental.Config.app, :host, "localhost")
+    Application.put_env(Instrumental.Config.app, :port, 4040)
+    Application.put_env(Instrumental.Config.app, :token, body)
+    {:ok, pid} = Connection.start_link
+    :timer.sleep(5000)
+    hello_msg = receive do
+      {:command, msg} -> msg
+      _ -> assert false
+    end
+    Logger.info "after receive #{inspect hello_msg}"
+    authenticate_msg = receive do
+      {:command, msg} -> msg
+      _ -> assert false
+    end
+    Logger.info "after receive #{inspect authenticate_msg}"
+    :timer.sleep(5000)
+    Logger.error "====================TEST================================"
+    Logger.info "gauge"
     I.gauge("elixir.gauge", 1)
+    Logger.info "gauge after"
+    bullshit_msg = receive do
+      {:command, msg} -> msg
+      _ -> assert false
+    end
+    Logger.info "after receive #{inspect bullshit_msg}"
+    gauge_msg = receive do
+      {:command, msg} -> msg
+      _ -> assert false
+    end
+    Logger.info "after receive #{inspect gauge_msg}"
+    # RegExp = "gauge elixir.gauge 1",
+    assert Regex.match?(~r/gauge elixir.gauge 1/, gauge_msg)
+    # case re:run(gauge_msg, RegExp) do
+    #   {match, Captured} -> assert true
+    #   nomatch -> assert false
+    # end
+    # assert ~r/gauge elixir.gauge 1/.run(gauge_msg)
     :timer.sleep(5000)
   end
 
-  test "sends increment correctly" do
-    I.increment("elixir.increment")
-    :timer.sleep(5000)
-  end
+  # test "sends increment correctly" do
+  #   I.increment("elixir.increment")
+  #   :timer.sleep(5000)
+  # end
 
-  test "sends time correctly" do
-    :ok = I.time("elixir.time", fn -> :timer.sleep(100) end)
-    :timer.sleep(5000)
-  end
+  # test "sends time correctly" do
+  #   :ok = I.time("elixir.time", fn -> :timer.sleep(100) end)
+  #   :timer.sleep(5000)
+  # end
 
-  test "sends notice correctly" do
-    :ok = I.notice("elixir test notice")
-    :timer.sleep(5000)
-  end  
+  # test "sends notice correctly" do
+  #   :ok = I.notice("elixir test notice")
+  #   :timer.sleep(5000)
+  # end  
 end
---
+++ test/test_server.exs
@@ -0,0 +1,73 @@
+defmodule KVServer do
+  use Application
+  require Logger
+
+  @doc false
+  def start(_type, test_process) do
+    import Supervisor.Spec
+    
+    children = [
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040, test_process]])
+    ]
+
+    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Starts accepting connections on the given `port`.
+  """
+  def accept(port, test_process) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                      [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket, test_process)
+  end
+
+  defp loop_acceptor(socket, test_process) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor,
+      fn -> serve(client, [], test_process) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket, test_process)
+  end
+
+  # def send_ok do
+  #   write_line("ok\nok\n", socket)
+  # end
+
+  defp serve(socket, commands, test_process) do
+    command = socket |> read_line()
+    Logger.info "COMMAND:: #{inspect command}"
+    new_commands = commands ++ [command]
+
+    if Regex.match?(~r/hello/, command) do
+      Logger.info "OK"
+      write_line("ok\n", socket)
+    end
+
+    if Regex.match?(~r/authenticate/, command) do
+      Logger.info "OK"
+      write_line("ok\n", socket)
+    end
+    
+    Logger.info "SEND TO TEST"
+    send(test_process, {:command, command})
+
+    serve(socket, commands ++ [command], test_process)
+  end
+
+  defp read_line(socket) do
+    Logger.error "read line"
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    Logger.error "read line after #{inspect data}"
+    data
+  end
+
+  defp write_line(line, socket) do
+    Logger.error "write line"
+    :gen_tcp.send(socket, line)
+    Logger.error "write line finished"
+  end
+end
